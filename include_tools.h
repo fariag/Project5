d00f48619c8b2c97eeef891f08e9def4f930ac62
fariag <54441892+fariag@users.noreply.github.com>
2019-12-15 21:29:20 +0100

    Create include_tools.h

---
+++ include_tools.h
@@ -0,0 +1,7 @@
+// Header file for tools.cpp
+
+#pragma once
+
+double* normalize(int* dist, int length, int n);
+int* distribution(double* array, int N, double limit, double dx);
+double difference(double* array1, double* array2, int n);
